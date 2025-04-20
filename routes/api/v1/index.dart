4dafd39e78fff691a79a9c33576a9cc8dfbb4e3c
Fulleni <fulleni@hotmail.com>
2025-04-20 16:54:43 +0100

    refactor(api): update welcome message comment
    - Simplify the comment above the welcome message
    - Remove reference to API version as it's already included in the response

--- routes/api/v1/index.dart
+++ routes/api/v1/index.dart
@@ -6,8 +6,8 @@ import 'package:dart_frog/dart_frog.dart';
 ///
 /// Returns a simple welcome message indicating the API is running.
 Response onRequest(RequestContext context) {
-  // You could potentially add more information here, like API version
-  // or links to documentation in a real application.
+  // You could potentially add more information here 
+  // like links to documentation.
   return Response.json(
     statusCode: HttpStatus.ok, // 200
     body: {'message': 'Welcome to the Headlines Toolkit API V1!'}, 
