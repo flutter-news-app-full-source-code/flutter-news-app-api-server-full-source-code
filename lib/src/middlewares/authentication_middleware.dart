ad36b3ecdc0c843ac75ffd5ec1015ef81fc4f28b
fulleni <fulleni@hotmail.com>
2025-05-16 14:45:02 +0100

    fix(auth): provide user in context
    - Provide non-nullable User
    - Simplifies user access

--- lib/src/middlewares/authentication_middleware.dart
+++ lib/src/middlewares/authentication_middleware.dart
@@ -119,7 +119,7 @@ Middleware requireAuthentication() {
       }
       // If user exists, proceed to the handler
       print('Authentication check passed for user: ${user.id}');
-      return handler(context);
+      return handler(context.provide<User>(() => user)); // Provide non-nullable User
     };
   };
 }
