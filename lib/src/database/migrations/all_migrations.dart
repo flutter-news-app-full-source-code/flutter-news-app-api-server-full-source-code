b5e201db42764570d04555b4ca5ae4dfa19115f6
fulleni <fulleni@hotmail.com>
2025-10-13 16:57:22 +0100

    feat(database): add migration for saved filters in remote config
    - Implement new migration for adding saved filters to remote config
    - Update all_migrations.dart to include the new migration

--- lib/src/database/migrations/all_migrations.dart
+++ lib/src/database/migrations/all_migrations.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_news_app_api_server_full_source_code/src/database/migration.dart';
 import 'package:flutter_news_app_api_server_full_source_code/src/database/migrations/20250924084800__refactor_ad_config_to_role_based.dart';
 import 'package:flutter_news_app_api_server_full_source_code/src/database/migrations/20251013000056_add_saved_filters_to_user_preferences.dart';
+import 'package:flutter_news_app_api_server_full_source_code/src/database/migrations/20251013000057_add_saved_filters_to_remote_config.dart';
 import 'package:flutter_news_app_api_server_full_source_code/src/services/database_migration_service.dart'
     show DatabaseMigrationService;
 
@@ -12,4 +13,5 @@ import 'package:flutter_news_app_api_server_full_source_code/src/services/databa
 final List<Migration> allMigrations = [
   RefactorAdConfigToRoleBased(),
   AddSavedFiltersToUserPreferences(),
+  AddSavedFiltersToRemoteConfig(),
 ];
