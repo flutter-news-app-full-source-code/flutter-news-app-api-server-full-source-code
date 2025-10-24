a65e33e21f418cc224aa0a4113ea01c7ca4ce192
fulleni <fulleni@hotmail.com>
2025-10-24 08:03:21 +0100

    style: format

--- lib/src/database/migrations/all_migrations.dart
+++ lib/src/database/migrations/all_migrations.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_news_app_api_server_full_source_code/src/database/migration.dart';
 import 'package:flutter_news_app_api_server_full_source_code/src/database/migrations/20250924084800__refactor_ad_config_to_role_based.dart';
-import 'package:flutter_news_app_api_server_full_source_code/src/database/migrations/20251024000000_add_logo_url_to_sources.dart';
 import 'package:flutter_news_app_api_server_full_source_code/src/database/migrations/20251013000056_add_saved_filters_to_user_preferences.dart';
 import 'package:flutter_news_app_api_server_full_source_code/src/database/migrations/20251013000057_add_saved_filters_to_remote_config.dart';
+import 'package:flutter_news_app_api_server_full_source_code/src/database/migrations/20251024000000_add_logo_url_to_sources.dart';
 import 'package:flutter_news_app_api_server_full_source_code/src/services/database_migration_service.dart'
     show DatabaseMigrationService;
 
