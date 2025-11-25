f44e32ce06cd9c5e8e380b203c0b4d79b73c7c17
fulleni <fulleni@hotmail.com>
2025-11-25 07:21:31 +0100

    build(serialization): generate

--- lib/src/models/push_notification/firebase_request_body.g.dart
+++ lib/src/models/push_notification/firebase_request_body.g.dart
@@ -6,59 +6,32 @@ part of 'firebase_request_body.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-FirebaseRequestBody _$FirebaseRequestBodyFromJson(Map<String, dynamic> json) =>
-    $checkedCreate('FirebaseRequestBody', json, ($checkedConvert) {
-      final val = FirebaseRequestBody(
-        message: $checkedConvert(
-          'message',
-          (v) => FirebaseMessage.fromJson(v as Map<String, dynamic>),
-        ),
-      );
-      return val;
-    });
-
 Map<String, dynamic> _$FirebaseRequestBodyToJson(
   FirebaseRequestBody instance,
-) => <String, dynamic>{'message': instance.message.toJson()};
-
-FirebaseMessage _$FirebaseMessageFromJson(Map<String, dynamic> json) =>
-    $checkedCreate('FirebaseMessage', json, ($checkedConvert) {
-      final val = FirebaseMessage(
-        token: $checkedConvert('token', (v) => v as String),
-        notification: $checkedConvert(
-          'notification',
-          (v) => FirebaseNotification.fromJson(v as Map<String, dynamic>),
-        ),
-        data: $checkedConvert(
-          'data',
-          (v) => PushNotificationPayload.fromJson(v as Map<String, dynamic>),
-        ),
-      );
-      return val;
-    });
+) => <String, dynamic>{
+  'stringify': instance.stringify,
+  'hashCode': instance.hashCode,
+  'message': instance.message.toJson(),
+  'props': instance.props,
+};
 
 Map<String, dynamic> _$FirebaseMessageToJson(FirebaseMessage instance) =>
     <String, dynamic>{
+      'stringify': instance.stringify,
+      'hashCode': instance.hashCode,
       'token': instance.token,
       'notification': instance.notification.toJson(),
       'data': instance.data.toJson(),
+      'props': instance.props,
     };
-
-FirebaseNotification _$FirebaseNotificationFromJson(
-  Map<String, dynamic> json,
-) => $checkedCreate('FirebaseNotification', json, ($checkedConvert) {
-  final val = FirebaseNotification(
-    title: $checkedConvert('title', (v) => v as String),
-    body: $checkedConvert('body', (v) => v as String),
-    image: $checkedConvert('image', (v) => v as String?),
-  );
-  return val;
-});
 
 Map<String, dynamic> _$FirebaseNotificationToJson(
   FirebaseNotification instance,
 ) => <String, dynamic>{
+  'stringify': instance.stringify,
+  'hashCode': instance.hashCode,
   'title': instance.title,
   'body': instance.body,
   'image': instance.image,
+  'props': instance.props,
 };
