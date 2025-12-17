5a5793a7a76ff2efd4e20e0aac2f50fd79db1bfa
fulleni <fulleni@hotmail.com>
2025-12-17 13:54:20 +0100

    chore: barrel

--- lib/src/services/analytics/analytics.dart
+++ lib/src/services/analytics/analytics.dart
@@ -1,4 +1,5 @@
 export 'analytics_metric_mapper.dart';
+export 'analytics_query_builder.dart';
 export 'analytics_reporting_client.dart';
 export 'analytics_sync_service.dart';
 export 'google_analytics_data_client.dart';
