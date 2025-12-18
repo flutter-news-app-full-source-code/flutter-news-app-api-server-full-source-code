d8c99ef33fcc29b051fd4aee22eede24a87395c6
fulleni <fulleni@hotmail.com>
2025-12-18 07:44:45 +0100

    build(serialization): sync

--- lib/src/models/analytics/google_analytics_request.g.dart
+++ lib/src/models/analytics/google_analytics_request.g.dart
@@ -6,24 +6,38 @@ part of 'google_analytics_request.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-RunReportRequest _$RunReportRequestFromJson(Map<String, dynamic> json) =>
-    RunReportRequest(
-      dateRanges: (json['dateRanges'] as List<dynamic>)
+RunReportRequest _$RunReportRequestFromJson(
+  Map<String, dynamic> json,
+) => $checkedCreate('RunReportRequest', json, ($checkedConvert) {
+  final val = RunReportRequest(
+    dateRanges: $checkedConvert(
+      'dateRanges',
+      (v) => (v as List<dynamic>)
           .map((e) => GARequestDateRange.fromJson(e as Map<String, dynamic>))
           .toList(),
-      dimensions: (json['dimensions'] as List<dynamic>?)
+    ),
+    dimensions: $checkedConvert(
+      'dimensions',
+      (v) => (v as List<dynamic>?)
           ?.map((e) => GARequestDimension.fromJson(e as Map<String, dynamic>))
           .toList(),
-      metrics: (json['metrics'] as List<dynamic>?)
+    ),
+    metrics: $checkedConvert(
+      'metrics',
+      (v) => (v as List<dynamic>?)
           ?.map((e) => GARequestMetric.fromJson(e as Map<String, dynamic>))
           .toList(),
-      dimensionFilter: json['dimensionFilter'] == null
+    ),
+    dimensionFilter: $checkedConvert(
+      'dimensionFilter',
+      (v) => v == null
           ? null
-          : GARequestFilterExpression.fromJson(
-              json['dimensionFilter'] as Map<String, dynamic>,
-            ),
-      limit: (json['limit'] as num?)?.toInt(),
-    );
+          : GARequestFilterExpression.fromJson(v as Map<String, dynamic>),
+    ),
+    limit: $checkedConvert('limit', (v) => (v as num?)?.toInt()),
+  );
+  return val;
+});
 
 Map<String, dynamic> _$RunReportRequestToJson(RunReportRequest instance) =>
     <String, dynamic>{
@@ -35,10 +49,13 @@ Map<String, dynamic> _$RunReportRequestToJson(RunReportRequest instance) =>
     };
 
 GARequestDateRange _$GARequestDateRangeFromJson(Map<String, dynamic> json) =>
-    GARequestDateRange(
-      startDate: json['startDate'] as String,
-      endDate: json['endDate'] as String,
-    );
+    $checkedCreate('GARequestDateRange', json, ($checkedConvert) {
+      final val = GARequestDateRange(
+        startDate: $checkedConvert('startDate', (v) => v as String),
+        endDate: $checkedConvert('endDate', (v) => v as String),
+      );
+      return val;
+    });
 
 Map<String, dynamic> _$GARequestDateRangeToJson(GARequestDateRange instance) =>
     <String, dynamic>{
@@ -47,34 +64,54 @@ Map<String, dynamic> _$GARequestDateRangeToJson(GARequestDateRange instance) =>
     };
 
 GARequestDimension _$GARequestDimensionFromJson(Map<String, dynamic> json) =>
-    GARequestDimension(name: json['name'] as String);
+    $checkedCreate('GARequestDimension', json, ($checkedConvert) {
+      final val = GARequestDimension(
+        name: $checkedConvert('name', (v) => v as String),
+      );
+      return val;
+    });
 
 Map<String, dynamic> _$GARequestDimensionToJson(GARequestDimension instance) =>
     <String, dynamic>{'name': instance.name};
 
 GARequestMetric _$GARequestMetricFromJson(Map<String, dynamic> json) =>
-    GARequestMetric(name: json['name'] as String);
+    $checkedCreate('GARequestMetric', json, ($checkedConvert) {
+      final val = GARequestMetric(
+        name: $checkedConvert('name', (v) => v as String),
+      );
+      return val;
+    });
 
 Map<String, dynamic> _$GARequestMetricToJson(GARequestMetric instance) =>
     <String, dynamic>{'name': instance.name};
 
 GARequestFilterExpression _$GARequestFilterExpressionFromJson(
   Map<String, dynamic> json,
-) => GARequestFilterExpression(
-  filter: GARequestFilter.fromJson(json['filter'] as Map<String, dynamic>),
-);
+) => $checkedCreate('GARequestFilterExpression', json, ($checkedConvert) {
+  final val = GARequestFilterExpression(
+    filter: $checkedConvert(
+      'filter',
+      (v) => GARequestFilter.fromJson(v as Map<String, dynamic>),
+    ),
+  );
+  return val;
+});
 
 Map<String, dynamic> _$GARequestFilterExpressionToJson(
   GARequestFilterExpression instance,
 ) => <String, dynamic>{'filter': instance.filter.toJson()};
 
 GARequestFilter _$GARequestFilterFromJson(Map<String, dynamic> json) =>
-    GARequestFilter(
-      fieldName: json['fieldName'] as String,
-      stringFilter: GARequestStringFilter.fromJson(
-        json['stringFilter'] as Map<String, dynamic>,
-      ),
-    );
+    $checkedCreate('GARequestFilter', json, ($checkedConvert) {
+      final val = GARequestFilter(
+        fieldName: $checkedConvert('fieldName', (v) => v as String),
+        stringFilter: $checkedConvert(
+          'stringFilter',
+          (v) => GARequestStringFilter.fromJson(v as Map<String, dynamic>),
+        ),
+      );
+      return val;
+    });
 
 Map<String, dynamic> _$GARequestFilterToJson(GARequestFilter instance) =>
     <String, dynamic>{
@@ -84,7 +121,12 @@ Map<String, dynamic> _$GARequestFilterToJson(GARequestFilter instance) =>
 
 GARequestStringFilter _$GARequestStringFilterFromJson(
   Map<String, dynamic> json,
-) => GARequestStringFilter(value: json['value'] as String);
+) => $checkedCreate('GARequestStringFilter', json, ($checkedConvert) {
+  final val = GARequestStringFilter(
+    value: $checkedConvert('value', (v) => v as String),
+  );
+  return val;
+});
 
 Map<String, dynamic> _$GARequestStringFilterToJson(
   GARequestStringFilter instance,
--- lib/src/models/analytics/mixpanel_request.g.dart
+++ lib/src/models/analytics/mixpanel_request.g.dart
@@ -6,17 +6,26 @@ part of 'mixpanel_request.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
+MixpanelSegmentationRequest _$MixpanelSegmentationRequestFromJson(
+  Map<String, dynamic> json,
+) => MixpanelSegmentationRequest(
+  projectId: json['project_id'] as String,
+  event: json['event'] as String,
+  fromDate: json['from_date'] as String,
+  toDate: json['to_date'] as String,
+  unit:
+      $enumDecodeNullable(_$MixpanelTimeUnitEnumMap, json['unit']) ??
+      MixpanelTimeUnit.day,
+);
+
 Map<String, dynamic> _$MixpanelSegmentationRequestToJson(
   MixpanelSegmentationRequest instance,
 ) => <String, dynamic>{
-  'stringify': instance.stringify,
-  'hashCode': instance.hashCode,
   'project_id': instance.projectId,
   'event': instance.event,
   'from_date': instance.fromDate,
   'to_date': instance.toDate,
   'unit': _$MixpanelTimeUnitEnumMap[instance.unit]!,
-  'props': instance.props,
 };
 
 const _$MixpanelTimeUnitEnumMap = {
@@ -26,16 +35,24 @@ const _$MixpanelTimeUnitEnumMap = {
   MixpanelTimeUnit.month: 'month',
 };
 
+MixpanelTopEventsRequest _$MixpanelTopEventsRequestFromJson(
+  Map<String, dynamic> json,
+) => MixpanelTopEventsRequest(
+  projectId: json['project_id'] as String,
+  event: json['event'] as String,
+  name: json['name'] as String,
+  fromDate: json['from_date'] as String,
+  toDate: json['to_date'] as String,
+  limit: (json['limit'] as num).toInt(),
+);
+
 Map<String, dynamic> _$MixpanelTopEventsRequestToJson(
   MixpanelTopEventsRequest instance,
 ) => <String, dynamic>{
-  'stringify': instance.stringify,
-  'hashCode': instance.hashCode,
   'project_id': instance.projectId,
   'event': instance.event,
   'name': instance.name,
   'from_date': instance.fromDate,
   'to_date': instance.toDate,
   'limit': instance.limit,
-  'props': instance.props,
 };
