e9cd480198b8c182cc56dd033db0109df2a3b711
fulleni <fulleni@hotmail.com>
2025-12-25 12:25:28 +0100

    test(payment): add unit test for AppleNotificationSubtype enum
    - Create new test file for AppleNotificationSubtype enum
    - Add test case to verify the expected values of the enum
    - Ensure all enum values are correctly defined and present

---
+++ test/src/enums/payment/apple_notification_subtype_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_news_app_api_server_full_source_code/src/enums/payment/apple_notification_subtype.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('AppleNotificationSubtype', () {
+    test('contains expected values', () {
+      expect(AppleNotificationSubtype.values, containsAll([
+        AppleNotificationSubtype.initialBuy,
+        AppleNotificationSubtype.resubscribe,
+        AppleNotificationSubtype.downgrade,
+        AppleNotificationSubtype.upgrade,
+        AppleNotificationSubtype.autoRenewEnabled,
+        AppleNotificationSubtype.autoRenewDisabled,
+      ]));
+    });
+  });
+}
