ef48603aa8d84c8135a815ba0dba9b5b32b3cbaf
fulleni <fulleni@hotmail.com>
2025-12-30 08:28:37 +0100

    refactor(email): remove stringify, hashCode and props properties
    - Removed stringify and hashCode properties from OneSignalEmailRequest and SendGridRequest models
    - Removed props property from all email request models
    - Simplified SendGridTo and SendGridFrom toJson methods

--- lib/src/models/email/onesignal_request.g.dart
+++ lib/src/models/email/onesignal_request.g.dart
@@ -9,12 +9,9 @@ part of 'onesignal_request.dart';
 Map<String, dynamic> _$OneSignalEmailRequestToJson(
   OneSignalEmailRequest instance,
 ) => <String, dynamic>{
-  'stringify': instance.stringify,
-  'hashCode': instance.hashCode,
   'app_id': instance.appId,
   'template_id': instance.templateId,
   'include_email_tokens': instance.includeEmailTokens,
   'email_subject': instance.emailSubject,
   'custom_data': instance.customData,
-  'props': instance.props,
 };
--- lib/src/models/email/sendgrid_request.g.dart
+++ lib/src/models/email/sendgrid_request.g.dart
@@ -9,37 +9,21 @@ part of 'sendgrid_request.dart';
 Map<String, dynamic> _$SendGridRequestToJson(
   SendGridRequest instance,
 ) => <String, dynamic>{
-  'stringify': instance.stringify,
-  'hashCode': instance.hashCode,
   'personalizations': instance.personalizations.map((e) => e.toJson()).toList(),
   'from': instance.from.toJson(),
   'template_id': instance.templateId,
-  'props': instance.props,
 };
 
 Map<String, dynamic> _$SendGridPersonalizationToJson(
   SendGridPersonalization instance,
 ) => <String, dynamic>{
-  'stringify': instance.stringify,
-  'hashCode': instance.hashCode,
   'to': instance.to.map((e) => e.toJson()).toList(),
   'subject': instance.subject,
   'dynamic_template_data': instance.dynamicTemplateData,
-  'props': instance.props,
 };
 
 Map<String, dynamic> _$SendGridToToJson(SendGridTo instance) =>
-    <String, dynamic>{
-      'stringify': instance.stringify,
-      'hashCode': instance.hashCode,
-      'email': instance.email,
-      'props': instance.props,
-    };
+    <String, dynamic>{'email': instance.email};
 
 Map<String, dynamic> _$SendGridFromToJson(SendGridFrom instance) =>
-    <String, dynamic>{
-      'stringify': instance.stringify,
-      'hashCode': instance.hashCode,
-      'email': instance.email,
-      'props': instance.props,
-    };
+    <String, dynamic>{'email': instance.email};
