cdc44cac3a0fb53680711465e836765f3b3864b1
fulleni <fulleni@hotmail.com>
2025-12-30 07:45:20 +0100

    build(serialization): sync

--- lib/src/models/email/onesignal_request.g.dart
+++ lib/src/models/email/onesignal_request.g.dart
@@ -6,24 +6,15 @@ part of 'onesignal_request.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-OneSignalEmailRequest _$OneSignalEmailRequestFromJson(
-  Map<String, dynamic> json,
-) => OneSignalEmailRequest(
-  appId: json['app_id'] as String,
-  templateId: json['template_id'] as String,
-  includeEmailTokens: (json['include_email_tokens'] as List<dynamic>)
-      .map((e) => e as String)
-      .toList(),
-  emailSubject: json['email_subject'] as String,
-  customData: json['custom_data'] as Map<String, dynamic>,
-);
-
 Map<String, dynamic> _$OneSignalEmailRequestToJson(
   OneSignalEmailRequest instance,
 ) => <String, dynamic>{
+  'stringify': instance.stringify,
+  'hashCode': instance.hashCode,
   'app_id': instance.appId,
   'template_id': instance.templateId,
   'include_email_tokens': instance.includeEmailTokens,
   'email_subject': instance.emailSubject,
   'custom_data': instance.customData,
+  'props': instance.props,
 };
--- lib/src/models/email/sendgrid_request.g.dart
+++ lib/src/models/email/sendgrid_request.g.dart
@@ -6,51 +6,40 @@ part of 'sendgrid_request.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-SendGridRequest _$SendGridRequestFromJson(Map<String, dynamic> json) =>
-    SendGridRequest(
-      personalizations: (json['personalizations'] as List<dynamic>)
-          .map(
-            (e) => SendGridPersonalization.fromJson(e as Map<String, dynamic>),
-          )
-          .toList(),
-      from: SendGridFrom.fromJson(json['from'] as Map<String, dynamic>),
-      templateId: json['template_id'] as String,
-    );
-
 Map<String, dynamic> _$SendGridRequestToJson(
   SendGridRequest instance,
 ) => <String, dynamic>{
+  'stringify': instance.stringify,
+  'hashCode': instance.hashCode,
   'personalizations': instance.personalizations.map((e) => e.toJson()).toList(),
   'from': instance.from.toJson(),
   'template_id': instance.templateId,
+  'props': instance.props,
 };
-
-SendGridPersonalization _$SendGridPersonalizationFromJson(
-  Map<String, dynamic> json,
-) => SendGridPersonalization(
-  to: (json['to'] as List<dynamic>)
-      .map((e) => SendGridTo.fromJson(e as Map<String, dynamic>))
-      .toList(),
-  subject: json['subject'] as String,
-  dynamicTemplateData: json['dynamic_template_data'] as Map<String, dynamic>,
-);
 
 Map<String, dynamic> _$SendGridPersonalizationToJson(
   SendGridPersonalization instance,
 ) => <String, dynamic>{
+  'stringify': instance.stringify,
+  'hashCode': instance.hashCode,
   'to': instance.to.map((e) => e.toJson()).toList(),
   'subject': instance.subject,
   'dynamic_template_data': instance.dynamicTemplateData,
+  'props': instance.props,
 };
 
-SendGridTo _$SendGridToFromJson(Map<String, dynamic> json) =>
-    SendGridTo(email: json['email'] as String);
-
 Map<String, dynamic> _$SendGridToToJson(SendGridTo instance) =>
-    <String, dynamic>{'email': instance.email};
-
-SendGridFrom _$SendGridFromFromJson(Map<String, dynamic> json) =>
-    SendGridFrom(email: json['email'] as String);
+    <String, dynamic>{
+      'stringify': instance.stringify,
+      'hashCode': instance.hashCode,
+      'email': instance.email,
+      'props': instance.props,
+    };
 
 Map<String, dynamic> _$SendGridFromToJson(SendGridFrom instance) =>
-    <String, dynamic>{'email': instance.email};
+    <String, dynamic>{
+      'stringify': instance.stringify,
+      'hashCode': instance.hashCode,
+      'email': instance.email,
+      'props': instance.props,
+    };
