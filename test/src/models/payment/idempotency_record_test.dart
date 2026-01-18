e70517ddefd096b4d15e6fb1753e31fca2baa0d6
Fulleni Flen <fulleni@hotmail.com>
2026-01-18 16:51:43 +0100

    test: update imports for idempotency models and services
    - Update import paths for IdempotencyRecord model from 'payment/idempotency_record.dart' to 'idempotency_record.dart'
    - Update import paths for IdempotencyService from 'payment/idempotency_service.dart' to 'idempotency_service.dart'

--- test/src/models/payment/idempotency_record_test.dart
+++ test/src/models/payment/idempotency_record_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_news_app_api_server_full_source_code/src/models/payment/idempotency_record.dart';
+import 'package:flutter_news_app_api_server_full_source_code/src/models/idempotency_record.dart';
 import 'package:test/test.dart';
 
 void main() {
--- test/src/services/payment/idempotency_service_test.dart
+++ test/src/services/payment/idempotency_service_test.dart
@@ -1,7 +1,7 @@
 import 'package:core/core.dart';
 import 'package:data_repository/data_repository.dart';
-import 'package:flutter_news_app_api_server_full_source_code/src/models/payment/idempotency_record.dart';
-import 'package:flutter_news_app_api_server_full_source_code/src/services/payment/idempotency_service.dart';
+import 'package:flutter_news_app_api_server_full_source_code/src/models/idempotency_record.dart';
+import 'package:flutter_news_app_api_server_full_source_code/src/services/idempotency_service.dart';
 import 'package:logging/logging.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
