9718e8514529452f09520eb93b68d41228a6aa91
Fulleni Flen <fulleni@hotmail.com>
2026-02-03 09:31:02 +0100

    build(serialization): sync

--- lib/src/models/push_notification/firebase_request_body.g.dart
+++ lib/src/models/push_notification/firebase_request_body.g.dart
@@ -8,30 +8,19 @@ part of 'firebase_request_body.dart';
 
 Map<String, dynamic> _$FirebaseRequestBodyToJson(
   FirebaseRequestBody instance,
-) => <String, dynamic>{
-  'stringify': instance.stringify,
-  'hashCode': instance.hashCode,
-  'message': instance.message.toJson(),
-  'props': instance.props,
-};
+) => <String, dynamic>{'message': instance.message.toJson()};
 
 Map<String, dynamic> _$FirebaseMessageToJson(FirebaseMessage instance) =>
     <String, dynamic>{
-      'stringify': instance.stringify,
-      'hashCode': instance.hashCode,
       'token': instance.token,
       'notification': instance.notification.toJson(),
       'data': instance.data.toJson(),
-      'props': instance.props,
     };
 
 Map<String, dynamic> _$FirebaseNotificationToJson(
   FirebaseNotification instance,
 ) => <String, dynamic>{
-  'stringify': instance.stringify,
-  'hashCode': instance.hashCode,
   'title': instance.title,
   'body': instance.body,
   'image': instance.image,
-  'props': instance.props,
 };
--- lib/src/models/push_notification/onesignal_request_body.g.dart
+++ lib/src/models/push_notification/onesignal_request_body.g.dart
@@ -9,13 +9,10 @@ part of 'onesignal_request_body.dart';
 Map<String, dynamic> _$OneSignalRequestBodyToJson(
   OneSignalRequestBody instance,
 ) => <String, dynamic>{
-  'stringify': ?instance.stringify,
-  'hash_code': instance.hashCode,
   'app_id': instance.appId,
   'include_player_ids': instance.includePlayerIds,
   'headings': instance.headings,
   'contents': ?instance.contents,
   'data': instance.data.toJson(),
   'big_picture': ?instance.bigPicture,
-  'props': instance.props,
 };
