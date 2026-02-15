10d316d7973c7c6bf00026bb6d84d67bb7645ebd
Fulleni Flen <fulleni@hotmail.com>
2026-02-15 08:32:32 +0100

    chore: misc

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,6 +1,7 @@
 export 'analytics/analytics.dart';
 export 'email/email.dart';
 export 'idempotency_record.dart';
+export 'media/media.dart';
 export 'push_notification/push_notification.dart';
 export 'request_id.dart';
 export 'reward/reward.dart';
