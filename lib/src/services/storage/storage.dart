97b90f0faf56309604534793bc8daa3ad53536ae
Fulleni Flen <fulleni@hotmail.com>
2026-03-11 08:04:26 +0100

    feat(storage): add initial storage module structure
    - Add storage module structure with models and services
    - Update .gitignore to include storage directory

---
+++ lib/src/services/storage/storage.dart
@@ -0,0 +1,6 @@
+export 'google_cloud_storage_service.dart';
+export 'i_storage_service.dart';
+export 'local_media_finalization_job_service.dart';
+export 'local_storage_service.dart';
+export 's3_storage_service.dart';
+export 'upload_token_service.dart';
