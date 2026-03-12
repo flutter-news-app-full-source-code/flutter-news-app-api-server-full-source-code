9c890ebae7cec1578532c7f8738db68fecd94096
Fulleni Flen <fulleni@hotmail.com>
2026-03-12 09:07:17 +0100

    chore: barrels

---
+++ lib/src/services/intelligence/intelligence.dart
@@ -0,0 +1,4 @@
+export 'batched_notification_selector.dart';
+export 'identity_resolution_service.dart';
+export 'intelligence_service.dart';
+export 'strategies/strategies.dart';
---
+++ lib/src/services/intelligence/strategies/strategies.dart
@@ -0,0 +1,3 @@
+export 'ai_strategy.dart';
+export 'ingestion_enrichment_strategy.dart';
+export 'single_enrichment_strategy.dart';
